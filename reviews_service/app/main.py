e89d1e134f2ddb92e2338a9e3d6dff6d66e145d1
Maxim Pylyov <pylyov.pmi32@yandex.ru>
2025-03-24 15:08:32 +0300

    Добавил volumes в Mongo, поправил review_serivce

--- reviews_service/app/main.py
+++ reviews_service/app/main.py
@@ -14,7 +14,7 @@ db = client.reviews_db
 reviews_collection = db.reviews
 
 
-class Review(BaseModel):
+class ReviewCreate(BaseModel):
     product_id: int
     user_id: int
     rating: int = Field(..., ge=1, le=5)  
@@ -29,8 +29,8 @@ class ReviewShow(BaseModel):
     created_at: datetime
 
 
-@app.post("/reviews/", response_model=Review)
-async def create_review(review: Review):
+@app.post("/reviews/", response_model=ReviewCreate)
+async def create_review(review: ReviewCreate):
     review_dict = review.dict()
     review_dict["_id"] = str(ObjectId())
     review_dict["created_at"] = datetime.utcnow()
@@ -40,13 +40,15 @@ async def create_review(review: Review):
 @app.get("/reviews/{product_id}", response_model=List[ReviewShow])
 async def get_reviews(product_id: int):
     reviews = await reviews_collection.find({"product_id": product_id}).to_list()
+    if not reviews:
+        raise HTTPException(status_code=404, detail="Не найдено отзывов для этого товара")
     return [ReviewShow(id=str(review["_id"]), **review) for review in reviews]
 
 @app.delete("/reviews/{review_id}")
 async def delete_review(review_id: str):
     result = await reviews_collection.delete_one({"_id": review_id})
     if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Review not found")
-    return {"detail": "Review deleted"}
+        raise HTTPException(status_code=404, detail="Отзыв не найден")
+    return {"message": "Отзыв удалён"}
 
 
